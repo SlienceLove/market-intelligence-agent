0faa04c9a321c0df21fc7af9a71b408868893a7c
Sliencelove <332062948@qq.com>
2026-07-31 09:49:32 +0800

    build: add agent solution structure

---
+++ src/MarketIntelligence.Agent.Worker/Program.cs
@@ -0,0 +1,7 @@
+using MarketIntelligence.Agent.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
